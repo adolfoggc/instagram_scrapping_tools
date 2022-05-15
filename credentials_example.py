eb10651dd198f793bb9a86bbaf5bc666a658d01b
Carlos Adolfo <adolfo.ggc@gmail.com>
2022-05-14 19:58:27 -0500

    Add example for credentials

---
+++ credentials_example.py
@@ -0,0 +1,7 @@
+#Hi!
+#You need to copy or rename this file to 'credentials.py' and change it's configuration
+
+#your instagram login and password
+login = 'login'
+password = 'PASSWORD'
+account= 'account_1'
