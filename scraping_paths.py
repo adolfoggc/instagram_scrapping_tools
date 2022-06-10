9f5ae08108c5fee1951302f204196ae813b43a71
Carlos Adolfo <adolfo.ggc@gmail.com>
2022-06-10 06:45:04 -0500

    Update paths

--- scraping_paths.py
+++ scraping_paths.py
@@ -6,7 +6,6 @@ button_after_login_xpath = '/html/body/div[1]/section/main/div/div/div/div/butto
 button_no_updates_xpath = '/html/body/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div/div[3]/button[2]'
 photo_grid_xpath = '/html/body/div[1]/div/div[1]/div/div[1]/div/div/div[1]/div[1]/section/main/div/div[3]/article/div[1]/div'
 #ABOUT PROFILE
-profile_info_xpath = '/html/body/div[1]/div/div[1]/div/div[1]/div/div/div[1]/div[1]/section/main/div/header/section'
 profile_name_xpath = '/html/body/div[1]/div/div[1]/div/div[1]/div/div/div[1]/div[1]/section/main/div/header/section/div[1]/h2'
 profile_real_name_xpath = '/html/body/div[1]/div/div[1]/div/div[1]/div/div/div[1]/div[1]/section/main/div/header/section/div[2]/span'
 #PROFILE NUMBERS
